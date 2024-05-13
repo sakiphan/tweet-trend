2897e08a0792bfa9c49cfdf5e01db0dbe66c8ac8
sakiphan <dursunsakiphan@gmail.com>
2024-05-13 20:57:55 +0300

    subnet-2 bug fix

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -70,7 +70,7 @@ resource "aws_subnet" "demo-public-subnet-01"{
 
 resource "aws_subnet" "demo-public-subnet-02"{
   vpc_id = aws_vpc.demo-vpc.id
-  cidr_block = "10.1.1.0/24"
+  cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-1b"
   tags = {
