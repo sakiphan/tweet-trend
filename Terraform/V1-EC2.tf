2d2da90cfa5e42e38c646a25829bf41ad942b0c4
sakiphan <dursunsakiphan@gmail.com>
2024-05-13 20:29:05 +0300

    security group bug fixed and route tables add

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -2,17 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "demo-web" {
+resource "aws_instance" "demo-server" {
   ami = "ami-07caf09b362be10b8"
   instance_type = "t2.micro"
   key_name = "devops-key"
+  subnet_id = aws_subnet.demo-public-subnet-01.id
+  security_groups = [ demo-sg ]
 }
 
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
-  vpc_id = aws_vpc.dpp-vpc.id 
+  vpc_id = aws_vpc.demo-vpc.id 
   
   ingress {
     description      = "SHH access"
@@ -78,3 +80,20 @@ resource "aws_internet_gateway" "demo-igw" {
   }
 }
 
+resource "aws_route_table" "demo-public-rt" {
+  vpc_id = aws_vpc.demo-vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-igw.id
+  }
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-01" {
+  subnet_id = aws_subnet.demo-public-subnet-01.id
+  route_table_id = aws_route_table.demo-public-rt.id
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-02" {
+  subnet_id = aws_subnet.demo-public-subnet-02.id
+  route_table_id = aws_route_table.demo-public-rt.id
+}
