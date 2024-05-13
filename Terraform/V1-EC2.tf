381fe1a4a1e1677bdba5c8ae7b8001eb657a1abc
sakiphan <dursunsakiphan@gmail.com>
2024-05-13 20:22:27 +0300

    ec2 subnet and vpc add

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -43,3 +43,31 @@ resource "aws_security_group" "demo-sg" {
 
   }
 }
+
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "demo-public-subnet-01"{
+  vpc_id = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "devops-subnet-01"
+  }
+}
+
+resource "aws_subnet" "demo-public-subnet-02"{
+  vpc_id = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "devops-subnet-02"
+  }
+}
+
