9b71c32b361384073c96d6d63e97afe0e2833453
sakiphan <dursunsakiphan@gmail.com>
2024-05-13 20:29:52 +0300

    subnet-02 region change

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -67,7 +67,7 @@ resource "aws_subnet" "demo-public-subnet-02"{
   vpc_id = aws_vpc.demo-vpc.id
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "devops-subnet-02"
   }
