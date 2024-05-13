27d5937f30acc6f745fbda7c59420d5650ff5a16
sakiphan <dursunsakiphan@gmail.com>
2024-05-13 20:10:55 +0300

    first commit

---
+++ Terraform/V1-EC2.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-web" {
+  ami = "ami-07caf09b362be10b8"
+  instance_type = "t2.micro"
+  key_name = "devops-key"
+}
+
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id 
+  
+  ingress {
+    description      = "SHH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+
+  }
+}
